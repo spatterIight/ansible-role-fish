09ae6b93fa233c25df91531a5b81154920a596f0
spatterlight <spatterlight@spatterlight.space>
2024-12-27 00:10:26 -0500

    Issue 1: Basic working draft

--- files/config.fish
+++ files/config.fish
@@ -39,13 +39,8 @@ end
 
 ## Starship prompt
 if status --is-interactive
-   source ("/usr/bin/starship" init fish --print-full-init | psub)
+   source ("/usr/local/bin/starship" init fish --print-full-init | psub)
 end
-
-
-## Advanced command-not-found hook
-source /usr/share/doc/find-the-command/ftc.fish
-
 
 ## Functions
 # Functions needed for !! and !$ https://github.com/oh-my-fish/plugin-bang-bang
