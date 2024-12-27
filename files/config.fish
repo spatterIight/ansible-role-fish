ffd1a4c44815b0169ea23f209e1061c47b3985f1
spatterlight <spatterlight@spatterlight.space>
2024-12-27 13:10:46 -0500

    Issue 1: Fixed autopair and done

--- files/config.fish
+++ files/config.fish
@@ -113,7 +113,7 @@ alias lt 'eza -aT --color=always --group-directories-first --icons' # tree listi
 alias l. 'eza -ald --color=always --group-directories-first --icons .*' # show only dotfiles
 
 # Replace some more things with better alternatives
-alias cat 'batcat --style header --style snip --style changes --style header'
+alias cat 'batcat --style header --style snip --style changes --style header --no-pager'
 if not test -x /usr/bin/yay; and test -x /usr/bin/paru
     alias yay 'paru'
 end
